71ef26ca757f787712367e29399b06f33b14e5f1
Tony <antony.branquinho@gmail.com>
2023-12-05 14:50:28 -0300

    de primeira

---
+++ sequencia impares/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sequencia_impares {
+    class Program {
+        static void Main(string[] args) {
+
+            int x, i;
+
+            Console.Write("Digite o valor de X: ");
+            x = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= x; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
